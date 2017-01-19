35cfbceec227aca4014a89c70b1ca1fdc0070c56
Yuchou Gan <yuchou.gan@nxp.com>
2017-01-19 11:55:35 +0200

    egl: define egl native types for non-x11
    (cherry picked from commit d8333e5f9bb8ee5065b46f7dd0a3777456c033e6)

--- thirdparty/khronos/EGL/eglplatform.h
+++ thirdparty/khronos/EGL/eglplatform.h
@@ -111,6 +111,7 @@ typedef intptr_t EGLNativeWindowType;
 
 #elif defined(USE_X11)
 
+#if defined(HAVE_X11)
 /* X11 (tentative)  */
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -118,6 +119,11 @@ typedef intptr_t EGLNativeWindowType;
 typedef Display *EGLNativeDisplayType;
 typedef Pixmap   EGLNativePixmapType;
 typedef Window   EGLNativeWindowType;
+#else
+typedef void *EGLNativeDisplayType;
+typedef void *EGLNativePixmapType;
+typedef void *EGLNativeWindowType;
+#endif
 
 #elif defined(__unix__)
 
