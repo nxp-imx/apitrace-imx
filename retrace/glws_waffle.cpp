11ef00d9669f7575cce6ab1782cd0e117f7a8cf5
Adrian Negreanu <adrian.negreanu@nxp.com>
2018-09-12 11:28:45 +0300

    retrace,waffle: use waffle error reporting api

--- retrace/glws_waffle.cpp
+++ retrace/glws_waffle.cpp
@@ -123,6 +123,26 @@ processEvents(void) {
     return true;
 }
 
+static void
+error_waffle(void)
+{
+    const struct waffle_error_info *info = waffle_error_get_info();
+    const char *code = waffle_error_to_string(info->code);
+
+    if (info->message_length > 0)
+        std::cerr << "error:"
+                  << " " << info->message << "."
+                  << " " << code << "(" << info->code << ")"
+                  << "\n"
+                  ;
+    else
+        std::cerr << "error:"
+                  << " " << code << "(" << info->code << ")"
+                  << "\n"
+                  ;
+}
+
+
 void
 init(void)
 {
@@ -157,15 +177,20 @@ init(void)
     }
 #endif
 
+    bool ok;
     Attributes<int32_t> waffle_init_attrib_list;
     waffle_init_attrib_list.add(WAFFLE_PLATFORM, waffle_platform);
     waffle_init_attrib_list.end(WAFFLE_NONE);
 
-    waffle_init(waffle_init_attrib_list);
+    ok = waffle_init(waffle_init_attrib_list);
+    if (!ok) {
+        error_waffle();
+        exit(1);
+    }
 
     dpy = waffle_display_connect(NULL);
     if (!dpy) {
-        std::cerr << "error: waffle_display_connect failed\n";
+        error_waffle();
         exit(1);
     }
 }
@@ -204,7 +229,7 @@ createVisual(bool doubleBuffer, unsigned samples, Profile profile) {
     }
 
     if (!waffle_display_supports_context_api(dpy, waffle_api)) {
-        std::cerr << "error: waffle_display_supports_context_api failed \n";
+        error_waffle();
         exit(1);
         return NULL;
     }
@@ -236,7 +261,7 @@ createVisual(bool doubleBuffer, unsigned samples, Profile profile) {
 
     cfg = waffle_config_choose(dpy, config_attrib_list);
     if (!cfg) {
-        std::cerr << "error: waffle_config_choose failed\n";
+        error_waffle();
         exit(1);
         return NULL;
     }
@@ -270,7 +295,7 @@ createContext(const Visual *visual, Context *shareContext,
 
     context = waffle_context_create(waffleVisual->config, share_context);
     if (!context) {
-        std::cerr << "error: failed to create " << waffleVisual->profile << " context.\n";
+        error_waffle();
         exit(1);
         return NULL;
     }
