f7d99c0df8ed5a605151635513373d5f905b2955
Adrian Negreanu <adrian.negreanu@nxp.com>
2019-09-27 12:59:31 +0300

    LocalWriter: make a writer ignorable
    (cherry picked from commit 2676a063bde30df6c1b5120831e73c1ed2a7c754)

--- lib/trace/trace_writer_local.cpp
+++ lib/trace/trace_writer_local.cpp
@@ -198,6 +198,7 @@ LocalWriter::open(void) {
 static uintptr_t next_thread_num = 1;
 
 static OS_THREAD_LOCAL uintptr_t thread_num;
+static OS_THREAD_LOCAL uint32_t ignored_num;
 
 void LocalWriter::checkProcessId(void) {
     if (m_file &&
@@ -286,6 +287,36 @@ void LocalWriter::flush(void) {
     }
     mutex.unlock();
 }
+
+bool LocalWriter::isIgnored(void) {
+    mutex.lock();
+    if (0==ignored_num){
+        mutex.unlock();
+        return false;
+    } else {
+        mutex.unlock();
+        return true;
+    }
+}
+
+void LocalWriter::beginIgnore(void) {
+    mutex.lock();
+    ++ignored_num;
+}
+
+void LocalWriter::endIgnore(void) {
+    mutex.unlock();
+}
+
+void LocalWriter::beginTrace(void) {
+    mutex.lock();
+    --ignored_num;
+}
+
+void LocalWriter::endTrace(void) {
+    mutex.unlock();
+}
+
 
 
 LocalWriter localWriter;
